67224720d7f09e8544ca01e5dbab27ec5c0574f7
Jonathan Hogervorst <jonathan@buzzera.com>
2024-12-13 16:44:06 +0100

    Day 13

---
+++ 2024/day13/part1.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+$regex = '/^Button A: X\+(?<a_x>\d+), Y\+(?<a_y>\d+)\nButton B: X\+(?<b_x>\d+), Y\+(?<b_y>\d+)\nPrize: X=(?<prize_x>\d+), Y=(?<prize_y>\d+)$/m';
+preg_match_all($regex, file_get_contents($argv[1]), $matches, PREG_SET_ORDER);
+
+$total_cost = 0;
+
+foreach ($matches as $m) {
+	$b = ($m["a_y"] * $m["prize_x"] - $m["a_x"] * $m["prize_y"]) / ($m["a_y"] * $m["b_x"] - $m["a_x"] * $m["b_y"]);
+	$a = ($m["prize_x"] - $m["b_x"] * $b) / $m["a_x"];
+
+	if (!is_int($a) || !is_int($b) || $a > 100 || $b > 100) {
+		continue;
+	}
+
+	$total_cost += $a * 3 + $b;
+}
+
+echo "Total cost: {$total_cost}\n";
